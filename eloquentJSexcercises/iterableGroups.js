8698457a4a0c4ae34b33614c5ea706cf112e49df
Adam <adam.j.menker@gmail.com>
2020-04-06 20:37:35 -0500

    iterable groups solution for eloquent js added

---
+++ eloquentJSexcercises/iterableGroups.js
@@ -0,0 +1,82 @@
+class Group{
+    constructor(){
+        this.group = []
+    }
+    add(value){
+        let test = 1;
+        for(let index of this.group){
+            if(value === index){
+                test = 0;
+            }
+        }
+        if(test === 1){
+            this.group.push(value);
+        }
+        else{console.log(`'${value}' already in set, item not added`);}
+    }
+    delete(value){
+        let test = 1;
+        for(let index of this.group){
+            if(value === index){
+                this.group = this.group.filter(function(index) {
+                    return index !== value;
+                })
+            }
+        }
+    }
+    has(value){
+        let test = 0;
+        for(let index of this.group){
+            if(value === index){
+                test = 1;
+            }
+        }
+        if(test === 1){
+            return true;
+        }
+        else{return false;}
+    }
+    static from(iterable){
+        let newGroup = new Group;
+        for(let index of iterable){
+            newGroup.add(index);
+        }
+        return newGroup;
+    }
+
+    [Symbol.iterator]() {
+        return new GroupIterator(this);
+    }
+
+}
+
+
+class GroupIterator {
+    constructor(group) {
+      this.group = group;
+      this.position = 0;
+    }
+  
+    next() {
+      if (this.position >= this.group.group.length) {
+        return {done: true};
+      } else {
+        let result = {value: this.group.group[this.position],
+                      done: false};
+        this.position++;
+        return result;
+      }
+    }
+  }
+
+
+
+let group1 = new Group;
+group1.add(1);
+group1.add(2);
+
+
+
+for(let property of group1){
+    console.log(`property --> ${property}`);
+}
--- test.js
+++ test.js
@@ -9,13 +9,18 @@ myObj[Symbol.iterator] = function(){
     let properties = Object.keys(myObj);
     let count = 0;
     let isDone = false;
+    let check = 0;
 
     let next = () =>{
-        if(count >= properties.length){
+        if(count >= properties.length - 1){
             isDone = true;
         }
+        if(check >= 1){
+            count++;
+        }
+        check++;
 
-        return {done: isDone, value: this[properties[count++]]};
+        return {done: isDone, value: this[properties[count]]};
     }
     return {next};
 };
