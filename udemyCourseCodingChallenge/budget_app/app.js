8d85a917b61919e530e176bf784795ac9884c099
Adam <adam.j.menker@gmail.com>
2020-04-14 00:03:34 -0500

    fixed bug that wasnt accepting expenses

--- udemyCourseCodingChallenge/budget_app/app.js
+++ udemyCourseCodingChallenge/budget_app/app.js
@@ -60,7 +60,7 @@ var UIController = (function(){
         inputValue: '.add__value',
         inputBtn: '.add__btn',
         incomeContainer: '.income__list',
-        expenseContainer: '.expense__list',
+        expenseContainer: '.expenses__list',
     }
 
     return {
@@ -84,7 +84,7 @@ var UIController = (function(){
                 html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
             }
 
-            
+            console.log(newHtml);
 
             // replace placeholder with data
 
