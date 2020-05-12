f3e42c441adec95386ff45f21dab6721e26ebdfa
Adam <adam.j.menker@gmail.com>
2020-05-11 19:24:31 -0500

    added some search button functionality

--- udemyCourseCodingChallenge/forkify/src/js/index.js
+++ udemyCourseCodingChallenge/forkify/src/js/index.js
@@ -1,4 +1,33 @@
-// Global app controller
-import num from './test';
-const x = 23;
-console.log(`I imported ${num} from another module called test.js! Variable x is ${x}`);
+import Search from './models/Search'
+
+// GLOBAL STATE OF THE APP
+// - search object
+// - current recipte object
+// - shopping list object
+// - liked recipes
+const state = {}
+
+const controlSearch = async () => {
+    // get query from view
+    const query = 'pizza' // TODO
+
+    if(query) {
+        // new search object and add ot state
+        state.search = new Search(query);
+
+        // prepare Ui for results 
+
+        // search for recipes
+        await state.search.getResults();
+
+        // render results on UI
+        console.log(state.search.result);
+    }
+}
+
+document.querySelector('.search').addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
+
+
---
+++ udemyCourseCodingChallenge/forkify/src/js/models/Search.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+// forkify-api.herokuapp.com
+
+export default class Search {
+    constructor(query) {
+        this.query = query;
+    }
+
+    async getResults(query) {
+        try{
+            const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${this.query}`);
+            this.result = res.data.recipes;
+            // console.log(this.result);
+        } catch(error){
+            alert(error);
+        }
+    }
+}
+
+
+
+
+
+
+
